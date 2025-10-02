d836ae7c58022c66ba16f9589527ceae0fed0ece
Abinash Mohapatra <abinashmohapatra@Mac-2.lan>
2025-10-02 09:49:15 +0530

    Exploring the HTTP methods

--- src/app.js
+++ src/app.js
@@ -2,6 +2,22 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Abinash", lastname: "Mohapatra" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data inserted succesfully!");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("User updated successfully!");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("User remove successfully!");
+});
+
 app.use("/hello", (req, res) => {
   res.send("Hello hello hello!");
 });
