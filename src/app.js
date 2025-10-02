2c7540718aa9505d1ab67a74481b1b693b286894
Abinash Mohapatra <abinashmohapatra@Mac-10.lan>
2025-10-02 14:11:05 +0530

    playing with route handlers and next()

--- src/app.js
+++ src/app.js
@@ -2,29 +2,31 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({ firstname: "Abinash", lastname: "Mohapatra" });
-});
-
-app.post("/user", (req, res) => {
-  res.send("Data inserted succesfully!");
-});
-
-app.patch("/user", (req, res) => {
-  res.send("User updated successfully!");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("User remove successfully!");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("Hello hello hello!");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Hello from test route!");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling the route user 01!!");
+    next();
+  },
+  [
+    (req, res, next) => {
+      console.log("Handlig the route user 02!!");
+      next();
+    },
+    (req, res, next) => {
+      console.log("Handlig the route user 03!!");
+      next();
+    },
+  ],
+  (req, res, next) => {
+    console.log("Handlig the route user 04!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handlig the route user 05!!");
+    res.send("Response 05...");
+  }
+);
 
 app.listen(7777, () => {
   console.log("Server is running on port 7777");
