508a8b52bf71f5763909f72101d99e7246950e0c
abinash458 <amohapatra458@gmail.com>
2025-10-22 15:10:00 +0530

    add fetchChat API

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,6 @@
 const socket = require("socket.io");
 const crypto = require("crypto");
+const { Chat } = require("../models/chat");
 
 const getSecretRoomId = (userId, targetUserId) => {
   return crypto
@@ -22,11 +23,40 @@ const initializeSocket = (server) => {
       console.log("Joining Room : " + roomId);
       socket.join(roomId);
     });
-    socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
-      const roomId = getSecretRoomId(userId, targetUserId);
-      console.log(firstName + " " + text);
-      io.to(roomId).emit("messageReceived", { firstName, text });
-    });
+
+    socket.on(
+      "sendMessage",
+      async ({ firstName, lastName, userId, targetUserId, text }) => {
+        try {
+          const roomId = getSecretRoomId(userId, targetUserId);
+          console.log(firstName + " " + text);
+
+          // save messages to db
+          let chat = await Chat.findOne({
+            participants: { $all: [userId, targetUserId] },
+          });
+
+          if (!chat) {
+            chat = await Chat({
+              participants: [userId, targetUserId],
+              messages: [],
+            });
+          }
+
+          chat.messages.push({
+            senderId: userId,
+            text,
+          });
+
+          await chat.save();
+
+          io.to(roomId).emit("messageReceived", { firstName, lastName, text });
+        } catch (error) {
+          console.log(error.message);
+        }
+      }
+    );
+
     socket.on("disconnect", () => {});
   });
 };
